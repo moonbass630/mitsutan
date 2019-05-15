efc53fd7fe0355c1ec60a491ef300ab48065e9f4
Mitsuki <moonbass630@gmail.com>
2019-05-15 13:36:39 +0900

    単語をJson形式のファイルを使って登録できるように修正

--- functions.php
+++ functions.php
@@ -22,16 +22,77 @@ add_action( 'admin_menu', function(){
       'manage_options',  //表示するユーザの権限
       'mitsutan',      //管理するスラッグ
       'mtt_getConfigPage',  //呼び出す関数名
-      'dashicons-book-alt',//ダッシュアイコンのURL（ニンジン）
+      'dashicons-book-alt',//ダッシュアイコンのURL（本）
       21                //メニュー位置
     );
 });
 
 function mtt_getConfigPage(){
-  print "fromみつタン";
+
+  // //入力フォームから値を取ってくる
+  // $sWordValue = $_POST['content'];
+
+  $html = '';
+
+  $html .= '<h2>みつタン</h2>';
+  $html .= '<form action="" method="POST" name="regform">';//"action"はpostの送信先。空だとformがあるファイルにpostの内容を送る
+
+  //保存ボタンが押下された時のみテキストファイルを作成し、単語を登録する
+  if(isset($_POST['save'])){
+    //入力フォームから値を取得  
+    $aEngWords = $_POST['eng'];
+    $aJaWords = $_POST['ja'];
+    //英単語=>日本語の連想配列を作成
+    $aaWords = array_combine($aEngWords, $aJaWords);
+    //キーが空の要素を削除する
+    unset($aaWords[null]);
+    //Json形式に変換する
+    $json = json_encode($aaWords);
+    //テキストファイルを作成する
+    file_put_contents(mtt_getFilePath().mtt_getFileName(), $json);
+  }
+
+  //Jsonファイルの内容を取得する
+  $sWordValue = file_get_contents(mtt_getFilePath().mtt_getFileName());
+  //Jsonファイルの内容をデコードして連想配列を格納する
+  $aaJsonImported = json_decode($sWordValue, 'ture');
+
+  //連想配列の数だけループして入力フォームを表示させるようにする
+  foreach($aaJsonImported as $key=>$value){
+    $html .= "<input type='text' id='eng' name='eng[]' value=$key>";
+    $html .= "<input type='text' id='ja' name='ja[]' value=$value>";
+    $html .= '<br>';
+  }
+  //一つ多めに入力フォームを表示させる
+  $html .= '<input type="text" id="eng" name="eng[]" value="">';
+  $html .= '<input type="text" id="ja" name="ja[]" value="">' ;
+
+  $html .= '<input type="submit" value="保存" name="save" onclick="checkForm()">';
+  $html .= '</form>';
+
+  //HTMLをprintする
+  print $html;
 }
 
-//javaScriptを読み込む
+
+
+//入力フォームの値を取ってくる関数
+function mtt_getWordValue(){
+  $sWordValue = $_POST['content'];
+  return $sWordValue;
+}
+
+function mtt_getFilePath(){
+  $sPath = "/home/moonbass/www/wp/wp-content/plugins/mitsutan/";
+  return $sPath;
+}
+
+function mtt_getFileName(){
+  $sFileName = "saved_word.txt";
+  return $sFileName;
+}
+
+//javaScriptとCSSのファイルを読み込む
 add_action('admin_enqueue_scripts', function(){
   wp_enqueue_script(
     'mitsu-common',//キューを登録する、”common”はいろんなところで使われる名前だから危険
@@ -39,17 +100,12 @@ add_action('admin_enqueue_scripts', function(){
     array(),
     false,
     true
+  ); 
+  wp_enqueue_style(
+    'mitsu-stylesheet',//キューを登録する、”common”はいろんなところで使われる名前だから危険
+    plugins_url('css/stylesheet.css', __FILE__)
   );
 });
 
-//CSSを読み込む
-add_action('admin_enqueue_style', function(){
-  wp_enqueue_style(
-    'mitsu-stylesheet',//キューを登録する、”common”はいろんなところで使われる名前だから危険
-    plugins_url('css/stylesheet.css', __FILE__),
-    array(),
-    false,
-    null
-  );
-});
+
 
